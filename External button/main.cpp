505fa19da939f6a4316e9f33182b773364134ee2
NgDKhanh <75005098+NgDKhanh@users.noreply.github.com>
2022-12-30 10:25:54 +0700

    Add files via upload

---
+++ External button/main.cpp
@@ -0,0 +1,198 @@
+#include <lvgl.h>
+#include <TFT_eSPI.h>
+/*If you want to use the LVGL examples,
+  make sure to install the lv_examples Arduino library
+  and uncomment the following line.
+#include <lv_examples.h>
+*/
+
+
+/*Change to your screen resolution*/
+static const uint16_t screenWidth  = 320;
+static const uint16_t screenHeight = 240;
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[ screenWidth * 10 ];
+
+lv_obj_t * scr1;
+lv_obj_t * scr2;
+
+
+
+TFT_eSPI tft = TFT_eSPI(screenWidth, screenHeight); /* TFT instance */
+
+#if LV_USE_LOG != 0
+/* Serial debugging */
+void my_print(const char * buf)
+{
+    Serial.printf(buf);
+    Serial.flush();
+}
+#endif
+
+/* Display flushing */
+void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
+{
+    uint32_t w = ( area->x2 - area->x1 + 1 );
+    uint32_t h = ( area->y2 - area->y1 + 1 );
+
+    tft.startWrite();
+    tft.setAddrWindow( area->x1, area->y1, w, h );
+    tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
+    tft.endWrite();
+
+    lv_disp_flush_ready( disp );
+}
+
+/*Read the touchpad*/
+void my_touchpad_read( lv_indev_drv_t * indev_driver, lv_indev_data_t * data )
+{
+    uint16_t touchX, touchY;
+
+    bool touched = tft.getTouch( &touchX, &touchY, 600 );
+
+    if( !touched )
+    {
+        data->state = LV_INDEV_STATE_REL;
+    }
+    else
+    {
+        data->state = LV_INDEV_STATE_PR;
+
+        /*Set the coordinates*/
+        data->point.x = touchX;
+        data->point.y = touchY;
+
+        Serial.print( "Data x " );
+        Serial.println( touchX );
+
+        Serial.print( "Data y " );
+        Serial.println( touchY );
+    }
+}
+
+uint8_t my_btn_read() {
+  if(digitalRead(34) == 0) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
+
+void button_read(lv_indev_drv_t * drv, lv_indev_data_t * data){
+    static uint32_t last_btn = 0;   /*Store the last pressed button*/
+    int btn_pr = my_btn_read();     /*Get the ID (0,1,2...) of the pressed button*/
+    if(btn_pr >= 0) {               /*Is there a button press? (E.g. -1 indicated no button was pressed)*/
+       last_btn = btn_pr;           /*Save the ID of the pressed button*/
+       data->state = LV_INDEV_STATE_PRESSED;  /*Set the pressed state*/
+    } else {
+       data->state = LV_INDEV_STATE_RELEASED; /*Set the released state*/
+    }
+
+    data->btn_id = last_btn;            /*Save the last button*/
+}
+
+
+static void btn_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * btn = lv_event_get_target(e);
+    if(code == LV_EVENT_CLICKED) {
+        static uint8_t cnt = 0;
+        cnt++;
+
+        /*Get the first child of the button which is the label and change its text*/
+        lv_obj_t * label = lv_obj_get_child(btn, 0);
+        lv_label_set_text_fmt(label, "Button: %d", cnt);
+    }
+}
+
+void ext_btn(void){
+  static lv_obj_t *btn = lv_btn_create(lv_scr_act());  
+  lv_obj_set_size(btn, 120, 40);
+  lv_obj_set_pos(btn, 0, 0);
+  //lv_btn_set_action(btn, LV_BTN_ACTION_CLICK, btn_click_action);
+  lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
+
+  lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
+  lv_label_set_text(label, "Button");                     /*Set the labels text*/
+  lv_obj_center(label);
+}
+
+void setup()
+{
+    pinMode(34, INPUT);
+    Serial.begin( 115200 ); /* prepare for possible serial debug */
+
+    String LVGL_Arduino = "Hello Arduino! ";
+    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+
+    Serial.println( LVGL_Arduino );
+    Serial.println( "I am LVGL_Arduino" );
+
+    lv_init();
+
+#if LV_USE_LOG != 0
+    lv_log_register_print_cb( my_print ); /* register print function for debugging */
+#endif
+
+    tft.begin();          /* TFT init */
+    tft.setRotation( 3 ); /* Landscape orientation, flipped */
+
+    /*Set the touchscreen calibration data,
+     the actual data for your display can be acquired using
+     the Generic -> Touch_calibrate example from the TFT_eSPI library*/
+    uint16_t calData[5] = { 275, 3620, 264, 3532, 1 };
+    tft.setTouch( calData );
+
+    lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * 10 );
+
+    /*Initialize the display*/
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init( &disp_drv );
+    /*Change the following line to your display resolution*/
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register( &disp_drv );
+
+    /*Initialize the (dummy) input device driver*/
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init( &indev_drv );
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register( &indev_drv );
+
+    static lv_indev_t *indev;
+    static lv_indev_drv_t indev_drv1;
+    lv_indev_drv_init(&indev_drv1);
+    indev_drv1.type = LV_INDEV_TYPE_BUTTON;
+    indev_drv1.read_cb = button_read;
+    indev = lv_indev_drv_register(&indev_drv1);
+
+    static lv_point_t points_array[] = { { 40, 40 } };
+    lv_indev_set_button_points(indev, points_array);
+
+
+#if 0
+    /* Create simple label */
+    lv_obj_t *label = lv_label_create( lv_scr_act() );
+    lv_label_set_text( label, LVGL_Arduino.c_str() );
+    lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
+#else
+    //scr1 = lv_obj_create(lv_scr_act());
+    ext_btn();
+
+    
+
+
+#endif
+    Serial.println( "Setup done" );
+}
+
+void loop()
+{
+    lv_timer_handler(); /* let the GUI do its work */
+    delay( 5 );
+}
